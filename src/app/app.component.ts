7aa9fcb78b7352fd582f97a1f11d52550e465226
Estrella-BF <gracielabenitesf@gmail.com>
2022-07-29 22:33:00 -0500

    feat: header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-declare var bootstrap: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'fe-onboarding-pyme';
-  pathName: string = '';
-  mostrarHeader: boolean = true;
+  title = 'header';
 
-  constructor(private router: Router) { }
+  constructor() { }
 
   ngOnInit() {
-    this.pushPageViewAlCambiarRuta();
-  }
-
-  pushPageViewAlCambiarRuta() {
-    this.router.events.subscribe((rutaActual) => {
-      if (rutaActual instanceof NavigationEnd) {
-        window.scroll(0, 0);
-        // se detecta que tenga query param
-        if (this.router.url === '/' || this.router.url.includes('/?') ) {
-          this.mostrarHeader = false;
-        } else {
-          this.mostrarHeader = true;
-        }
-      }
-    });
   }
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  menuList = [
+    {
+      id: '1',
+      name: 'INICIO'
+    },
+    {
+      id: '1',
+      name: 'NOSOTROS'
+    },
+    {
+      id: '1',
+      name: 'MATERIALES'
+    },
+    {
+      id: '1',
+      name: 'FOTOS'
+    },
+    {
+      id: '1',
+      name: 'CONTÁCTANOS'
+    },
+  ];
+
+  menuListStyle = {
+    color: '#030a01',
+    fontSize: '1.2rem',
+  };
+
+  specialButton = {
+    active: true,
+    backgroundColor: '#eb8c2a',
+    borderRadius: '1em',
+    color: '#030a01',
+    fontSize: '1.2rem',
+    fontWeight: '600',
+    padding: '0.5em 0.8em',
+    text: 'COTIZAR ALFOMBRA'
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
