4611cfdb1fce3c54ea4f2155fc16b768dfb19fd3
gbenites <gracielabenitesf@gmail.com>
2022-09-05 22:39:18 -0500

    creando contact us

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,34 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+/*   {
+    path: '',
+    component: LoginHazteClienteComponent,
+  }, */
+  {
+    path: 'ofertas',
+    component: ContactUsComponent
+  }
+/*   {
+    path: '**',
+    component: LoginHazteClienteComponent,
+  }, */
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CarouselComponent,
-    FooterComponent
+    FooterComponent,
+    ContactUsComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   bootstrap: [AppComponent],
