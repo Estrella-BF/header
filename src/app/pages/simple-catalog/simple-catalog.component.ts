97750b02e83ed0c511491e7725d86cf0c77c23ad
gbenites <gracielabenitesf@gmail.com>
2022-10-05 22:55:54 -0500

    materiales

--- src/app/pages/simple-catalog/simple-catalog.component.ts
+++ src/app/pages/simple-catalog/simple-catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IImage } from './simple-catalog.interface';
 
 @Component({
   selector: 'app-simple-catalog',
@@ -11,5 +12,28 @@ export class SimpleCatalogComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  images: IImage[] = [
+    {
+      url: './assets/temp/catalog-test-image.jpeg',
+      name: 'HANDTUFTED DE OVINO EN ALT. DE 14 y 16 MM'
+    },
+    {
+      url: './assets/temp/catalog-test-image-2.jpeg',
+      name: 'TELAR DE OVINO'
+    },
+    {
+      url: './assets/temp/catalog-test-image.jpeg',
+      name: 'ALPACA SURI'
+    },
+    {
+      url: './assets/temp/catalog-test-image.jpeg',
+      name: 'PATCHWORK CUERO DE VACA'
+    },
+    {
+      url: './assets/temp/catalog-test-image.jpeg',
+      name: 'SHAGGY DE OVINO EN ALT. DE 25 y 35 MM'
+    }
+  ]
 
 }
